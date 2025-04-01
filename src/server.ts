9d31dfec57490455440eb2de8250ab911ce28e4a
Arnav Gupta <guptaarn@msu.edu>
2025-04-01 14:30:38 -0400

    login signup home basic

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,12 @@ app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
 });
 
-app.get('/', (req,res) => {
+app.get('/login', (req,res) => {
+  res.sendFile('static/index.html', { root: __dirname });
+})
+app.get('/signup', (req,res) => {
+  res.sendFile('static/index.html', { root: __dirname });
+})
+app.get('/home', (req,res) => {
   res.sendFile('static/index.html', { root: __dirname });
 })

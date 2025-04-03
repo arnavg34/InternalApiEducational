1b0b72bc1f09ee4378aa34fd651623fbd1c374c6
Arnav Gupta <guptaarn@msu.edu>
2025-04-03 17:17:04 -0400

    very basic login manuever

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'static')));
 const distPath = path.resolve(__dirname.includes('dist') ? __dirname : path.join(__dirname, '..', 'dist'));
-console.log('DIST FOLDER:', distPath);
-app.use('/dist', express.static(distPath));
+console.log('DIST FOLDER:', distPath)app.use('/dist', express.static(distPath));
 // Routes
 app.use('/api/items', itemRoutes);
 
--- src/static/login/login.ts
+++ src/static/login/login.ts
@@ -6,8 +6,29 @@ type User = {
 const user: User = {email: "5", password: "Hi"}
 
 document.getElementById('login-button')?.addEventListener("click", () =>{
-    console.log("hey")
+    let emailInput = document.getElementById('email') as HTMLInputElement;
+    let emailOutput = emailInput?.value;
+    console.log()
+    console.log(emailOutput);
+    let passwordInput = document.getElementById('email') as HTMLInputElement;
+    let passwordOutput = passwordInput?.value; 
+    console.log(passwordOutput);
+    let emailSpan = document.getElementById("email-span") as HTMLInputElement;
+    let passwordSpan = document.getElementById("password-span") as HTMLInputElement;
+    if(passwordOutput === ""){
+        passwordSpan.innerHTML = `Please Enter a Password`
+    }
+    else{
+        passwordSpan.innerHTML = `The password is: ${passwordOutput}`
+    }  
+    if(emailOutput === ""){
+        emailSpan.innerHTML = `Please Enter a Email`
+    }    
+    else{
+        emailSpan.innerHTML = `The email is: ${emailOutput}`
+    }  
 });
 document.getElementById('signup-button')?.addEventListener("click", () =>{
     console.log("hey")
 });
+
